42f7fbfd7a0496c6e5bb37b93a9861cff96ff974
nildiert <niljordan23@gmail.com>
2019-06-17 21:39:23 +0000

    [FEATURE] Added file 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,11 +7,22 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t **copy;
-	copy = list;
+	listint_t *copy;
+	/* listint_t **temp; */
+	copy = *list;
 	while(copy)
 	{
-		
+		printf("%d : ",copy->n);
+		copy = copy->next;
 	}
 	print_list(*list);
+}
+
+
+void insertion_swap(listint_t *item_1, listint_t *item_2)
+{
+	listint_t *temp = NULL;
+
+	temp = malloc(sizeof(listint_t));
+	
 }

1362cf3dfd059b55af6983cf2dcac40ad5769f92
nildiert <niljordan23@gmail.com>
2019-06-17 16:26:46 +0000

    [FEATURE] Added bubble sort algoritm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,8 +1,30 @@
 #include "sort.h"
-
+/**
+ * bubble_sort - Bubble sort algoritm
+ *
+ * @array: Size of the array
+ * @size: Size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
+	unsigned int i, j = 0;
+	int temp = 0;
 	(void)array;
 	(void)size;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 ; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+
+	}
+
 
 }
